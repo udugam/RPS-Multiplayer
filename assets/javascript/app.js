df27f68a45de9864c3f2fb429411d0d901ec3a36
udugam <ryan.udugam@gmail.com>
2018-07-05 00:06:30 -0400

    added basic html structure, bootstrap, and setup firebase database

---
+++ assets/javascript/app.js
@@ -0,0 +1,12 @@
+var config = {
+    apiKey: "AIzaSyAZbpkdIHFWLpui315shwOre7Kkpnn_7B4",
+    authDomain: "rps-multiplayer-30b7b.firebaseapp.com",
+    databaseURL: "https://rps-multiplayer-30b7b.firebaseio.com",
+    projectId: "rps-multiplayer-30b7b",
+    storageBucket: "",
+    messagingSenderId: "496452426059"
+};
+  
+firebase.initializeApp(config);
+
+var database = firebase.database();
