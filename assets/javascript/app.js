bf9acad0caa2f6e7fa253571fd526358a25885e3
udugam <ryan.udugam@gmail.com>
2018-07-05 23:24:36 -0400

    successfully adding a maximum of two players. Added init logic upon page load

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,21 +6,48 @@ var config = {
     storageBucket: "",
     messagingSenderId: "496452426059"
 };
-  
 firebase.initializeApp(config);
 
+//Declare global variables
 var database = firebase.database();
+var playerCount = 0; 
 
-//Declares a listener for the a change of values in the root directory of the firebase
-database.ref().on("value", function(snapshot) {    
+function initDatabase(databaseRef) {
+    database
+        .ref()
+        .once('value')
+        .then(function(snapshot) {
+            if (!snapshot.child('players').exists()) {
+                database.ref().set({
+                    numPlayers: 0
+                })
+            }
+        })
+}
 
+//Declares a listener for the change of values in the players path of the database
+database.ref().on("value", function(snapshot) {
+    playerCount = snapshot.val().numPlayers;
 })
 
 //Declares a click listener for when the player name is submitted
 $('#submitName').on('click', function() {
     var name = $('#playerName').val().trim();
+    var count = playerCount;
 
-    database.ref().set({
-        playerName: name
-    })
+    if (count < 2) {
+        count++;
+        database.ref('players').push({
+            playerName: name,
+            wins: 0,
+            losses: 0,
+            ties: 0,
+            selection: ""
+        })
+        database.ref().update({
+            numPlayers: count
+        })
+    }
 })
+
+initDatabase(database);
