6ddf63ef3cd247a0f38d4b6af425713db4c566d7
udugam <ryan.udugam@gmail.com>
2018-07-07 18:19:40 -0400

    database check ad logic on load of page working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -22,15 +22,47 @@ function checkDatabase() {
             //If there are no players in the database, call emptyGame function
             if(snapshot.val()===null) {
                 emptyGame();
+            } else if (snapshot.child('player1').exists() && !snapshot.child('player2').exists())  { //Else if player1 exists and player2 does not, call playerWaiting(playerNum) and missingPlayer(playerNum) functions 
+                playerWaiting('player1', snapshot.val().player1)
+                playerMissing('player2');
+            } else if (snapshot.child('player2').exists() && !snapshot.child('player1').exists()) { //Else if player2 exists and player1 does not, call playerWaiting(playerNum) and missingPlayer(playerNum) functions 
+                playerWaiting('player2', snapshot.val().player2)
+                playerMissing('player1');
+            } else if (snapshot.child('player1').exists() && snapshot.child('player2').exists()) { //Else if both players exist, call gameInProgress
+                populatePlayerData('player1',snapshot.val().player1);
+                populatePlayerData('player2',snapshot.val().player2);
+                startGame();
             }
-
-            //Else if player1 is missing, call missingPlayer(playerNum) function 
-
         })
 }
 
 function emptyGame() {
     $('#announcement-topic').text("Game Empty");
     $('#announcement').text("No one is here to play. Enter your name on either side and wait for another player.")
+    $('#player1').html("<input>");
+    $('#player2').html("<input>");
 }
+
+function playerWaiting(player, data) {
+    $('#announcement-topic').text("Oponent Ready");
+    $('#announcement').text("Enter your name to start playing");
+    $('#'+player).text(data.name+" is ready to play");
+}
+
+function playerMissing(player) {
+    $('#announcement-topic').text("Oponent Ready");
+    $('#announcement').text("Enter your name to start playing");
+    $('#'+player).html("<input>");
+}
+
+function populatePlayerData(player, data) {
+    $('#'+player).text(data.name);
+}
+
+function startGame() {
+    $('#announcement-topic').text("Choose Rock, Paper, or Scissors");
+    $('#announcement').text("Use the left and right arrows to make you selection, then press Submit");
+}
+
+
 
